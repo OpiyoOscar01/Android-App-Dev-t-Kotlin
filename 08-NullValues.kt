2d5cba7dedc93b96baaa57a107c4f0b75083335b
OPIYO OSCAR <148503309+OpiyoOscar01@users.noreply.github.com>
2025-01-11 21:46:26 +0300

    Covered concepts of null in Kotlin

---
+++ 08-NullValues.kt
@@ -0,0 +1,27 @@
+// Null values in Kotlin:
+// Kotlin's type system prevents null pointer exceptions by distinguishing between nullable and non-nullable types.
+// Use '?' for nullable types, '?:' (Elvis operator) for providing default values, and safe calls (?.) to handle null values.
+
+fun handleNullable(value: String?): String {
+    return value?.uppercase() ?: "Default Value"
+}
+
+fun main() {
+    val name: String? = null;
+    if (name != null) {
+        println("Your name is $name")
+        println("Your name is ${name.uppercase()}")
+        println(name.length)
+    } else {
+        println("Your name is not found")
+    }
+    name?.let {
+        println("Your name is $it")
+        println("Your name is ${it.uppercase()}")
+        println(it.length)
+    }
+    var message=name?: println("Your name is not found")
+    println(message)
+    println(handleNullable(null))
+    println(handleNullable("Hello"))
+}
