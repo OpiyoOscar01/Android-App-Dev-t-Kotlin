38c028504e7f04602c153e0b3f21952f227384f5
OPIYO OSCAR <148503309+OpiyoOscar01@users.noreply.github.com>
2025-02-13 12:41:02 +0300

    Covered for loop in Kotlin

---
+++ ForLoop.kt
@@ -0,0 +1,20 @@
+fun main(){
+    println("Looping from 1 upto 5")
+    for (i in 1..5){
+        println(i)
+    }
+    println("Looping from 1 until 4 using until keyword");
+    for(i in 1 until 5){
+        println(i)
+    }
+    println("Looping from 10 downto 4 using downTo keyword");
+
+    for (i in 10 downTo 1){
+        println(i)
+    }
+    println("Looping through a given range with a step size.")
+    for (i in 1..10 step 3){
+        println(i);
+    }
+
+}
